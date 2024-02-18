8bcdea80426925476f78c9eab3e07b70d5671c86
HuyDV <huydv3@rikkeisoft.com>
2024-02-19 00:44:13 +0700

    update

--- push-image.sh
+++ push-image.sh
@@ -4,8 +4,8 @@ USERNAME='huydv'
 PASSWORD='12345678aA@'
 IMAGE=$REGISTRY/redis-cluster
 echo 'Build image...'
-docker build -t $IMAGE .
-echo 'Login Nexus'
-docker login $REGISTRY -u $USERNAME -p $PASSWORD
-echo 'Push image '$IMAGE
-docker push $IMAGE:latest
+sudo docker build -t $IMAGE .
+# echo 'Login Nexus'
+# docker login $REGISTRY -u $USERNAME -p $PASSWORD
+# echo 'Push image '$IMAGE
+# docker push $IMAGE:latest
